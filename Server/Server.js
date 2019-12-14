0d284aed3c45078b4cff3285bad59dc1b6b0314d
unknown <dishantdua8956@gmail.com>
2019-12-14 19:14:20 +0530

    first step for deploy

---
+++ Server/Server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+var bodyParser = require('body-parser');
+var arr=  require('../db.json')
+require('dotenv').config()
+app.use(cors());
+ app.use(bodyParser.json()); 
+app.post('/add' ,(req,res) =>{
+    console.log(req.body)
+    const find = arr.find((info) => info.email ===req.body.email && info.password ===req.body.password);
+    // console.log(find)
+    if(find){
+        return res.json({
+            message : 'done'
+        })
+    }
+    else{
+        return res.json({
+            message : 'not done'
+        })
+    }
+});
+const PORT = process.env.PORT
+app.listen(PORT, () =>{
+    console.log('Connected');
+})
+
