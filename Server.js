c7c9743c593c668eac9e87c7a369d8ec9e907a76
unknown <dishantdua8956@gmail.com>
2019-12-14 20:49:40 +0530

    template

--- Server.js
+++ Server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 var bodyParser = require('body-parser');
-var arr=  require('./db.json')
+var arr=  require('./db.json');
+const path = require('path');
 require('dotenv').config()
 app.use(cors());
  app.use(bodyParser.json()); 
+ 
 app.post('/add' ,(req,res) =>{
     console.log(req.body)
     const find = arr.find((info) => info.email ===req.body.email && info.password ===req.body.password);
@@ -21,6 +23,9 @@ app.post('/add' ,(req,res) =>{
         })
     }
 });
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+})
 const PORT = process.env.PORT || 3007
 app.listen(PORT, () =>{
     console.log('Connected');
